c669a3286c6a5e1fcff75ae7561ec3b299e8dd9d
Mozzarth <moisescaicedo15@gmail.com>
2021-11-15 11:48:45 -0500

    ecosystem.config.js update

--- ecosystem.config.example.js
+++ ecosystem.config.example.js
@@ -7,9 +7,15 @@ module.exports = {
       max_restarts: 50,
       watch: false,
       env: {
-        NODE_ENV: "yourEnv",
-        NODE_HOST: 'localhost',
-        NODE_PORT: 3010,
+        NODE_APP_ENCODER: "mysecretjwt",
+        SECRET_SALT: 12,
+
+        NODE_DB_HOST: "127.0.0.1",
+        NODE_DB_USER: "YOUR_USER",
+        NODE_DB_PASS: "YOUR_PASS",
+        NODE_DB_PORT: "3307",
+        NODE_DB_DATABASE: "MY_DATABASE",
+        NODE_PORT: 3000
       },
       script: "index.js",
       cwd: "./dist/"
