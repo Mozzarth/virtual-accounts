8c6060c9e3021a6ad6cbe1fdc865d4e0ce7369a8
Mozzarth <moisescaicedo15@gmail.com>
2021-11-22 18:11:08 -0500

    ADD certifade SSL

--- src/server/server/server.ts
+++ src/server/server/server.ts
@@ -1,5 +1,8 @@
 import express, { Application } from "express";
 import { CONFIG } from "../config/config";
+import https = require('https');
+import path = require('path');
+import fs = require('fs');
 
 export class Server {
 
@@ -19,14 +22,24 @@ export class Server {
         return this.instance || (this.instance = new this())
     }
 
-    public async start() : Promise<void> {
+    public async start(): Promise<void> {
         try {
-            return new Promise((res,rej)=>{
-                this.app.listen(Number(this.port), () => {
-                    // console.log(`Server running on http://${this.host}:${this.port}`)
-                    console.log(`Server running on http://localhost:${this.port}`)
-                    res()
-              })
+            return new Promise((res, rej) => {
+                https.createServer({
+                    cert: fs.readFileSync(path.join(__dirname, "../config/Cert/localhost-cert.crt")),
+                    key: fs.readFileSync(path.join(__dirname, "../config/Cert/localhost-key.pem"))
+                }, this.app)
+                    .listen(Number(this.port), () => {
+                        console.log(`Server running on http://localhost:${this.port}`)
+                        res()
+                    })
+
+
+                // this.app.listen(Number(this.port), () => {
+                //     // console.log(`Server running on http://${this.host}:${this.port}`)
+                //     console.log(`Server running on http://localhost:${this.port}`)
+                //     res()
+                // })
             })
         } catch (error) { throw error }
     }
