0dc098d596f8b164811d6990eb19b7e3dabbff94
Moises David Caicedo <moisescaicedo15@gmail.com>
2022-03-02 18:23:14 -0500

    Cambio la manera en la que se envia el error

--- src/server/shared/middleware/handle-error.midd.ts
+++ src/server/shared/middleware/handle-error.midd.ts
@@ -1,13 +1,13 @@
 // import { ErrorUserRootAlreadyExist } from "../../../app/usecase/user/delete/domain/user-exists.error"
-import { Request, Response, NextFunction} from "express"
+import { Request, Response, NextFunction } from "express"
 
 export function handleErrorMiddleware(error: Error, req: Request, res: Response, next: NextFunction) {
-   //TODO Realizar los respectivos cambios para crear un diccionaio de errores
+    //TODO Realizar los respectivos cambios para crear un diccionaio de errores
 
     // console.log(error instanceof ErrorUserRootAlreadyExist)
     // console.log(error.name)
     // console.log(error.message)
     // console.log(error.stack)
     console.log(error)
-    return res.status(500).send(JSON.stringify(error))
+    return res.status(500).send({ err: error, message: error.message, errString: JSON.stringify(error) })
 }
